91fd30332979b2805a818a7a8ab03c863f31dc6a
Durgesh Thakur <52100227+durgesh12345@users.noreply.github.com>
2023-08-08 08:00:55 +0530

    Add files via upload
    Make repo as public

---
+++ src/main/java/com/example/cread2/Cread2Application.java
@@ -0,0 +1,13 @@
+package com.example.cread2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cread2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cread2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cread2/Cread2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cread2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cread2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
